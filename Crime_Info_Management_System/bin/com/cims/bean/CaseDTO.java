a31dc33bb335bb291c1d0a0cb5a32f57b133cdd1
Sirajuddin135 <shaiksirajuddin135@gmail.com>
2022-09-30 23:16:42 +0530

    day4

--- Crime_Info_Management_System/bin/com/cims/bean/CaseDTO.java
+++ Crime_Info_Management_System/bin/com/cims/bean/CaseDTO.java
@@ -1,51 +1,29 @@
 package com.cims.bean;
 
 public class CaseDTO {
-	private String victimName;
-	private String suspectName;
-	private int criminalId;
 	private String date;
-	
+	private String policeStation;
 	private int caseId;
 	private String typeOfCrime;
-	
+	private String desc;
+	private String victimName;
+	private int criminalId;
+	private String criminalName;
 	private String status;
 
 	public CaseDTO() {}
-	
-	public CaseDTO(String victimName, String suspectName, int criminalId, String date, int caseId, String typeOfCrime,
-			String status) {
-		this.victimName = victimName;
-		this.suspectName = suspectName;
-		this.criminalId = criminalId;
+
+	public CaseDTO(String date, String policeStation, int caseId, String typeOfCrime, String desc, String victimName,
+			int criminalId, String criminalName, String status) {
 		this.date = date;
+		this.policeStation = policeStation;
 		this.caseId = caseId;
 		this.typeOfCrime = typeOfCrime;
-		this.status = status;
-	}
-
-	public String getVictimName() {
-		return victimName;
-	}
-
-	public void setVictimName(String victimName) {
+		this.desc = desc;
 		this.victimName = victimName;
-	}
-
-	public String getSuspectName() {
-		return suspectName;
-	}
-
-	public void setSuspectName(String suspectName) {
-		this.suspectName = suspectName;
-	}
-
-	public int getCriminalId() {
-		return criminalId;
-	}
-
-	public void setCriminalId(int criminalId) {
 		this.criminalId = criminalId;
+		this.criminalName = criminalName;
+		this.status = status;
 	}
 
 	public String getDate() {
@@ -54,6 +32,14 @@ public class CaseDTO {
 
 	public void setDate(String date) {
 		this.date = date;
+	}
+
+	public String getPoliceStation() {
+		return policeStation;
+	}
+
+	public void setPoliceStation(String policeStation) {
+		this.policeStation = policeStation;
 	}
 
 	public int getCaseId() {
@@ -72,6 +58,38 @@ public class CaseDTO {
 		this.typeOfCrime = typeOfCrime;
 	}
 
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public String getVictimName() {
+		return victimName;
+	}
+
+	public void setVictimName(String victimName) {
+		this.victimName = victimName;
+	}
+
+	public int getCriminalId() {
+		return criminalId;
+	}
+
+	public void setCriminalId(int criminalId) {
+		this.criminalId = criminalId;
+	}
+
+	public String getCriminalName() {
+		return criminalName;
+	}
+
+	public void setCriminalName(String criminalName) {
+		this.criminalName = criminalName;
+	}
+
 	public String getStatus() {
 		return status;
 	}
@@ -82,7 +100,8 @@ public class CaseDTO {
 
 	@Override
 	public String toString() {
-		return "CaseDTO [victimName=" + victimName + ", suspectName=" + suspectName + ", criminalId=" + criminalId
-				+ ", date=" + date + ", caseId=" + caseId + ", typeOfCrime=" + typeOfCrime + ", status=" + status + "]";
+		return "CaseDTO [date=" + date + ", policeStation=" + policeStation + ", caseId=" + caseId + ", typeOfCrime="
+				+ typeOfCrime + ", desc=" + desc + ", victimName=" + victimName + ", criminalId=" + criminalId
+				+ ", criminalName=" + criminalName + ", status=" + status + "]";
 	}
 }
--- Crime_Info_Management_System/bin/com/cims/dao/CaseDaoImpl.java
+++ Crime_Info_Management_System/bin/com/cims/dao/CaseDaoImpl.java
@@ -4,24 +4,27 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import com.cims.bean.Case;
 import com.cims.bean.CaseDTO;
 import com.cims.bean.Criminal;
+import com.cims.bean.CriminalDTO;
+import com.cims.exceptions.CaseException;
+import com.cims.exceptions.CriminalException;
 import com.cims.utility.CIMSDB;
 
 public class CaseDaoImpl implements CaseDao {
 	@Override
 	public String registerACase(Case c, Criminal criminal) {
-		
 		String message = "Case not filed...";
 		
 		try(Connection conn = CIMSDB.provideConnection()) {
 			PreparedStatement ps1 = conn.prepareStatement("insert into cases(date, place, typeOfCrime, victims, description, suspect) values(?,?,?,?,?,?)");
-			PreparedStatement ps2 = conn.prepareStatement("insert into criminal(name, age, gender, address, identificationMarks, areaOfCrime, typeOfCrime) values(?,?,?,?,?,?,?)");
-			PreparedStatement ps3 = conn.prepareStatement("insert into criminal_case_status(caseId, criminalId, status) values(?,?,?)");
 			
 			ps1.setString(1, c.getDate());
 			ps1.setString(2, c.getPlace());
@@ -33,6 +36,8 @@ public class CaseDaoImpl implements CaseDao {
 			int x = ps1.executeUpdate();
 			
 			if(x > 0) {
+				PreparedStatement ps2 = conn.prepareStatement("insert into criminal(name, age, gender, address, identificationMarks, areaOfCrime, typeOfCrime) values(?,?,?,?,?,?,?)");
+
 				ps2.setString(1, criminal.getName());
 				ps2.setInt(2, criminal.getAge());
 				ps2.setString(3, criminal.getGender());
@@ -44,6 +49,8 @@ public class CaseDaoImpl implements CaseDao {
 				int y = ps2.executeUpdate();
 				
 				if(y > 0) {
+					PreparedStatement ps3 = conn.prepareStatement("insert into criminal_case_status(caseId, criminalId, status) values(?,?,?)");
+					
 					ps1 = conn.prepareStatement("select * from cases where caseId = (select max(caseId) from cases)");
 					
 					ResultSet rs1 = ps1.executeQuery();
@@ -87,29 +94,197 @@ public class CaseDaoImpl implements CaseDao {
 	}
 
 	@Override
-	public List<CaseDTO> getCasesByCriminalNames(String suspectName) {
+	public List<CaseDTO> getCasesByCriminalNames(String criminalName) throws CaseException {
 		List<CaseDTO> caseList = new ArrayList<>();
+		
+		try(Connection conn = CIMSDB.provideConnection()) {
+			PreparedStatement ps1 = conn.prepareStatement("select * from criminal where name = ?");
+			
+			ps1.setString(1, criminalName);
+			
+			ResultSet rs1 = ps1.executeQuery();
+			
+			while(rs1.next()) {
+				int criminalId = rs1.getInt("criminalId");
+				
+				PreparedStatement ps2 = conn.prepareStatement("select * from criminal_case_status where criminalId = ?");
+				
+				ps2.setInt(1, criminalId);
+				
+				ResultSet rs2 = ps2.executeQuery();
+				
+				if(rs2.next()) {
+					int caseId = rs2.getInt("caseId");
+					String status = rs2.getString("status");
+					
+					PreparedStatement ps3 = conn.prepareStatement("select * from cases where caseId = ?");
+					
+					ps3.setInt(1, caseId);
+					
+					ResultSet rs3 = ps3.executeQuery();
+					
+					if(rs3.next()) {
+						String date = rs3.getString("date");
+						String place = rs3.getString("place");
+						String typeOfCrime = rs3.getString("typeOfCrime");
+						String description = rs3.getString("description");
+						String victimName = rs3.getString("victims");
+						
+						CaseDTO caseDTO = new CaseDTO(date, place, caseId, typeOfCrime, description, victimName, criminalId, criminalName, status);
+						
+						caseList.add(caseDTO);
+					}
+				}
+			}
+			
+			if(caseList.isEmpty()) {
+				System.out.println("No case exists with criminal name: " + criminalName);
+			}
+		} catch(SQLException se) {
+			throw new CaseException("No case exists with criminal name: " + criminalName);
+		}
 		
 		return caseList;
 	}
 
 	@Override
-	public List<CaseDTO> getCriminalsByCrimeType(String typeOfCrime) {
-		List<CaseDTO> criminalsList = new ArrayList<>();
+	public List<CriminalDTO> getCriminalsByCrimeType(String typeOfCrime) throws CriminalException {
+		List<CriminalDTO> criminalsList = new ArrayList<>();
+		
+		try(Connection conn = CIMSDB.provideConnection()) {
+			PreparedStatement ps1 = conn.prepareStatement("select * from criminal where typeOfCrime = ?");
+			
+			ps1.setString(1, typeOfCrime);
+			
+			ResultSet rs1 = ps1.executeQuery();
+			
+			while(rs1.next()) {
+				int criminalId = rs1.getInt("criminalId");
+				String criminalName = rs1.getString("name");
+				int age = rs1.getInt("age");
+				String gender = rs1.getString("gender");
+				String identificationMarks = rs1.getString("identificationMarks");
+				String address = rs1.getString("address");
+				String areaOfCrime = rs1.getString("areaOfCrime");
+				
+				PreparedStatement ps2 = conn.prepareStatement("select * from criminal_case_status where criminalId = ?");
+				
+				ps2.setInt(1, criminalId);
+				
+				ResultSet rs2 = ps2.executeQuery();
+				
+				if(rs2.next()) {
+					int caseId = rs2.getInt("caseId");
+					String status = rs2.getString("status");
+					
+					PreparedStatement ps3 = conn.prepareStatement("select * from cases where caseId = ?");
+					
+					ps3.setInt(1, caseId);
+					
+					ResultSet rs3 = ps3.executeQuery();
+					
+					if(rs3.next()) {
+						String description = rs3.getString("description");
+						String victimName = rs3.getString("victims");
+						
+						CriminalDTO criminalDTO = new CriminalDTO(criminalId, criminalName, age, gender, identificationMarks, address, areaOfCrime, typeOfCrime, description, caseId, victimName, status);
+						
+						criminalsList.add(criminalDTO);
+					}
+				}
+			}
+		} catch(SQLException se) {
+			throw new CriminalException("No criminal record exists " + typeOfCrime + " case");
+		}
 		
 		return criminalsList;
 	}
 
 	@Override
-	public String updateCaseStatus(int caseId) {
+	public String updateCaseStatus(int caseId) throws CaseException {
 		String message = "Invalid case id...";
+		
+		try(Connection conn = CIMSDB.provideConnection()) {
+			PreparedStatement ps1 = conn.prepareStatement("select * from criminal_case_status where caseId = ?");
+			ps1.setInt(1, caseId);
+			
+			ResultSet rs1 = ps1.executeQuery();
+			
+			if(rs1.next()) {
+				String status = rs1.getString("status");
+	
+				if(status.equals("solved")) {
+					message = "Status already updated...";
+				} else {
+					PreparedStatement ps2 = conn.prepareStatement("update criminal_case_status set status = 'solved' where caseId = ?");
+					
+					ps2.setInt(1, caseId);
+					
+					int x = ps2.executeUpdate();
+					
+					if(x > 0) {
+						message = "Status updated successfully";
+					}
+				}
+			}
+		} catch (SQLException e) {
+			throw new CaseException(e.getMessage());
+		}
 		
 		return message;
 	}
 
 	@Override
-	public List<Integer> getTotalCaseByArea(String areaName) {
-		List<Integer> totalCaseDetails = new ArrayList<>();
+	public List<String> getTotalCaseByArea(String areaName) {
+		List<String> totalCaseDetails = new ArrayList<>();
+		
+		try(Connection conn = CIMSDB.provideConnection()) {
+			int solvedCases = 0, unsolvedCases = 0, currentMonthRecordedCases = 0;
+			
+			PreparedStatement ps1 = conn.prepareStatement("select * from cases where place = ?");
+			
+			ps1.setString(1, areaName);
+			
+			ResultSet rs1 = ps1.executeQuery();
+			
+			while(rs1.next()) {
+				int caseId = rs1.getInt("caseId");
+				String date1 = rs1.getString("date");
+		
+				String pattern = "yyyy-MM-dd";
+				SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+				String date2 = simpleDateFormat.format(new Date());
+	
+				LocalDate parseDate1 = LocalDate.parse(date1);
+				LocalDate parseDate2 = LocalDate.parse(date2);
+				
+				System.out.println(parseDate1);
+				System.out.println(parseDate2);
+				if(parseDate1.getMonth() == parseDate2.getMonth() && parseDate1.getYear() == parseDate2.getYear()) {
+					currentMonthRecordedCases++;
+				}
+				
+				PreparedStatement ps2 = conn.prepareStatement("select status from criminal_case_status where caseId = ?");
+
+				ps2.setInt(1, caseId);
+				
+				ResultSet rs2 = ps2.executeQuery();
+				
+				if(rs2.next()) {
+					if(rs2.getString("status").equals("solved")) {
+						solvedCases++;
+					} else {
+						unsolvedCases++;
+					}
+				}
+			}
+			
+			totalCaseDetails.add("Total solved cases: " + solvedCases);
+			totalCaseDetails.add("Total unsolved cases: " + unsolvedCases);
+			totalCaseDetails.add("Current month total cases: " + currentMonthRecordedCases);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 		return totalCaseDetails;
 	}
